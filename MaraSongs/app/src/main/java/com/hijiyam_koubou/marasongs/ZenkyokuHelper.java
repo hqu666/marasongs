95f9ecb07b109e7a6411ae1a1c77bc6bac01c155
hiroomi kuwayama <hqu666@gmail.com>
2019-09-14 21:40:54 +0900

    最近追加リスト：準備ブロックでdbファイルの作成と更新；テーブル置換え

--- MaraSongs/app/src/main/java/com/hijiyam_koubou/marasongs/ZenkyokuHelper.java
+++ MaraSongs/app/src/main/java/com/hijiyam_koubou/marasongs/ZenkyokuHelper.java
@@ -1,9 +1,11 @@
 package com.hijiyam_koubou.marasongs;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 public class ZenkyokuHelper extends SQLiteOpenHelper {
@@ -13,15 +15,15 @@ public class ZenkyokuHelper extends SQLiteOpenHelper {
 
 	public ZenkyokuHelper(Context context , String dFn) {					//アーティスト名の置き換えリスト
 		super(context, dFn, null, DB_VERSION);
-		final String TAG = "ZenkyokuHelper[ZenkyokuHelper]";
-		String dbMsg= "開始;";/////////////////////////////////////
+		final String TAG = "ZenkyokuHelper";
+		String dbMsg= "[ZenkyokuHelper]";
 		try{
-			dbMsg="getPackageCodePath="+context.getPackageCodePath();/////////////////////////////////////
+			dbMsg +="getPackageCodePath="+context.getPackageCodePath();
 			rContext = context;						//第１引数; context ;読出し元;データベースを所有するコンテキストオブジェクトを指定します。
 			dbName = dFn;							//第２引数; fileName ;データベースファイルの名前です。この引数にnullを指定すると、データベースはメモリー上に作られます。
 			dbMsg += " , db=" + dbName;
 			dbMsg +=",バージョン="+DB_VERSION;	//第4引数; version ;データベースのバージョンを指定します。
-	//		myLog(TAG,dbMsg);
+			myLog(TAG,dbMsg);
 		}catch (Exception e) {
 			myErrorLog(TAG,dbMsg + "で"+e.toString());
 		}
@@ -29,35 +31,43 @@ public class ZenkyokuHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {		// table create
-		final String TAG = "onCreate[ZenkyokuHelper]";
-		String dbMsg= "開始;";/////////////////////////////////////
+		final String TAG = "onCreate";
+		String dbMsg= "[ZenkyokuHelper];";/////////////////////////////////////
 		try{
+			dbMsg +="ファイル名= "+ dbName;/////////////////////////////////////
 			String tName = rContext.getResources().getString(R.string.zenkyoku_table);
-			dbMsg="テーブル名= "+ tName;/////////////////////////////////////
+			if(dbName.equals( rContext.getResources().getString(R.string.zenkyoku_file)) ||
+					   dbName.equals( rContext.getResources().getString(R.string.kari_file))
+			){ }else{
+				dbMsg +="は全曲リスト以外なので";
+				tName = dbName.replace(".db" , "");
+			}
+			dbMsg += ",テーブル名= "+ tName;/////////////////////////////////////
+
 			String tSet = "create table " + tName +" (" +							//テーブル名；artist_rw_table
 					"_id integer primary key autoincrement not null, "+ 			//作成したリストの連番
-					"	AUDIO_ID text not null," +		//1.元々のID
-					"	SORT_NAME text not null," +		//2.ALBUM_ARTISTを最短化して大文字化
-					"	ARTIST text not null," +		//3.artist;クレジットアーティスト名
-					"	ALBUM_ARTIST text, " +			//4.album_artist
-					"	ALBUM text, " +					//5.album
-					"	TRACK text, " +					//6.track;					//トラックNo,
-					"	TITLE text, " +					//7.title;					//タイトル
-					"	DURATION text, " +				//8.duration;				//再生時間
-					"	YEAR text, " +					//9.MediaStore.Audio.Media.YEAR
-					"	DATA text, " +					//10.data;					//URI
-					"	MODIFIED text, " +				//11.idkousinnbi = cur.getColumnIndex(MediaStore.Audio.Media.DATE_MODIFIED);
-					"	COMPOSER text, " +				//12.idcomposer = cur.getColumnIndex(MediaStore.Audio.Media.COMPOSER);
-					"	LAST_YEAR text " +				//13.MediaStore.Audio.Albums.LAST_YEAR
+					"AUDIO_ID text not null," +		//1.元々のID
+					"SORT_NAME text not null," +		//2.ALBUM_ARTISTを最短化して大文字化
+					"ARTIST text not null," +		//3.artist;クレジットアーティスト名
+					"ALBUM_ARTIST text, " +			//4.album_artist
+					"ALBUM text, " +					//5.album
+					"TRACK text, " +					//6.track;					//トラックNo,
+					"TITLE text, " +					//7.title;					//タイトル
+					"DURATION text, " +				//8.duration;				//再生時間
+					"YEAR text, " +					//9.MediaStore.Audio.Media.YEAR
+					"DATA text, " +					//10.data;					//URI
+					"MODIFIED text, " +				//11.idkousinnbi = cur.getColumnIndex(MediaStore.Audio.Media.DATE_MODIFIED);
+					"COMPOSER text, " +				//12.idcomposer = cur.getColumnIndex(MediaStore.Audio.Media.COMPOSER);
+					"LAST_YEAR text " +				//13.MediaStore.Audio.Albums.LAST_YEAR
 						");";
 /*
 	 				"	id text, "+ 										//MediaStore.Audio.Media._ID	updateに必要
 					"	BOOKMARK text, " +				//idbOOKMARK = cur.getColumnIndex(MediaStore.Audio.Media.BOOKMARK);			//APIL8
 					"	ALBUM_ART text, " +				//MediaStore.Audio.Albums.ALBUM_ART
 */
-			dbMsg="tSet= "+tSet;/////////////////////////////////////
+			dbMsg +=",tSet= "+tSet;/////////////////////////////////////
 			db.execSQL(tSet);
-	//		myLog(TAG,dbMsg);
+			myLog(TAG,dbMsg);
 		}catch (Exception e) {
 			myErrorLog(TAG,dbMsg + "で"+e.toString());
 		}
@@ -68,11 +78,46 @@ public class ZenkyokuHelper extends SQLiteOpenHelper {
 		final String TAG = "onUpgrade[ZenkyokuHelper]";
 		String dbMsg= "開始;";/////////////////////////////////////
 		try{
+			onCreate(db);
 			myLog(TAG,dbMsg);
 		}catch (Exception e) {
 			myErrorLog(TAG,dbMsg + "で"+e.toString());
 		}
 	}
+
+
+//	public void ColumnCheck( Context context , Cursor carsor) {
+//		final String TAG = "VersatilityDBHelper";
+//		String dbMsg = "[ZenkyokuHelper]";
+//		try {
+//
+//			dbMsg += "carsor = " + carsor.getCount() + "件";
+//			if(carsor.moveToFirst()){
+//				int colCount = carsor.getColumnCount();
+//				dbMsg = "," + colCount + "列";
+//				colNames =  "id INTEGER PRIMARY KEY AUTOINCREMENT,";
+//				for (int i = 0; i < colCount; i++) {
+//					dbMsg += "(" + i + ")";
+//					String colName = carsor.getColumnName(i);
+//					dbMsg += colName;
+//					colNames += colName  + " STRING";
+//					if(i < (colCount  - 1)){
+//						colNames += ",";
+////					}else{
+////						colNames += ")";
+//					}
+//				}
+//			}
+//
+//			myLog(TAG, dbMsg);
+//		} catch (Exception e) {
+//			myErrorLog(TAG ,  dbMsg + "で" + e);
+//		}
+//	}
+
+
+
+
 	///////////////////////////////////////////////////////////////////////////////////
 	public static void myLog(String TAG , String dbMsg) {
 		Util UTIL = new Util();
